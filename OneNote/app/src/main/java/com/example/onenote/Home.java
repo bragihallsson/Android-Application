9e0baa93653572d1bbdea8120fe87aaa407fb88e
bragihallsson <61268495+bragihallsson@users.noreply.github.com>
2020-06-14 18:58:38 +0200

    Upload

---
+++ OneNote/app/src/main/java/com/example/onenote/Home.java
@@ -0,0 +1,93 @@
+package com.example.onenote;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class Home extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    Adapter adapter;
+    TextView noItemText;
+    SimpleDatabase simpleDatabase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        noItemText = findViewById(R.id.noItemText);
+        simpleDatabase = new SimpleDatabase(this);
+        List<Note> allNotes = simpleDatabase.getAllNotes();
+        recyclerView = findViewById(R.id.allNotesList);
+
+        if(allNotes.isEmpty()){
+            noItemText.setVisibility(View.VISIBLE);
+        }else {
+            noItemText.setVisibility(View.GONE);
+            displayList(allNotes);
+        }
+
+    }
+
+    private void displayList(List<Note> allNotes) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this,allNotes);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add){
+            Toast.makeText(this, "Add New Note", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this,AddNote.class);
+            startActivity(i);
+        }
+        if(item.getItemId() == R.id.loction){
+            Toast.makeText(this, "Testing Your Location", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this,PermissionsActivity.class);
+            startActivity(i);
+        }
+        if(item.getItemId() == R.id.test){
+            Toast.makeText(this, "Trying Notifications", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this,TryNotification.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<Note> getAllNotes = simpleDatabase.getAllNotes();
+        if(getAllNotes.isEmpty()){
+            noItemText.setVisibility(View.VISIBLE);
+        }else {
+            noItemText.setVisibility(View.GONE);
+            displayList(getAllNotes);
+        }
+
+
+    }
+
+}
---
+++ OneNote/app/src/main/java/com/example/onenote/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.onenote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    int SPLASH_TIME = 3000; //This is 3 seconds
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iv=(ImageView)findViewById(R.id.img);
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.blink_anim);
+        iv.startAnimation(animation);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(MainActivity.this, Home.class);
+                startActivity(mySuperIntent);
+
+                //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
+                finish();
+
+            }
+        }, SPLASH_TIME);
+
+    }
+}
